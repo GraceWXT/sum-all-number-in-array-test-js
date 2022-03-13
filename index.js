9d92c4fdd771cd3c18708100516e82fa8d837c27
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-13 00:29:15 +0000

    completed a funciton that sums all the numbers in an array.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function sumItems(array) {
+  let sum = 0;
+  for (let num of array) {
+    if (Array.isArray(num)) {
+      sum += sumItems(num);
+    } else {
+      sum += num;
+    }
+  }
+  return sum;
   // Sum all the numbers in the array
 }
 
